90fc18b362b31c6de8c96e85994f3df64a7b5b41
FatimaNoorAhmed <fatimamansoor.fm30@gmail.com>
2025-03-22 23:59:26 +0500

    Fixed Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-  base: process.env.VITE_BASE_PATH || "Job_Portal_Final"
-  
+  base: process.env.VITE_BASE_PATH || "Job_Portal_Fianl",
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    }
+  }
 });
